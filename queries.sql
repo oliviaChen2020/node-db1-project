7a08b18f0e89e4596b8e3f6b991ac07915857482
Olivia <olivia@macbook-pro.myfiosgateway.com>
2020-12-07 16:56:19 -0500

    Writing basic queries done

--- queries.sql
+++ queries.sql
@@ -1,16 +1,22 @@
 -- Database Queries
 
 -- Find all customers with postal code 1010
+--select * from Customers where PostalCode is 1010
 
 -- Find the phone number for the supplier with the id 11
+--select Phone from Suppliers where SupplierId is 11
 
 -- List first 10 orders placed, sorted descending by the order date
+--select * from orders order by orderdate desc limit 10
 
 -- Find all customers that live in London, Madrid, or Brazil
+--select * from Customers where City ="London" or City ="Madrid" or City ="Brazil" 
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+--insert into customers (customername, contactname, address, city, postalcode, country) values ('The Shire', 'Bilbo Baggins', '1 Hobbit-Hole','Bag End', '111', 'Middle Earth') ;
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
+--update Customers set postalcode = "11122" where contactname = "Bilbo Baggins"
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
